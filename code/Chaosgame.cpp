2d6ea88a65049f2476219f7f3f68e28981e215ef
admin <admin@BCS-Ubuntu-18.lrccd.ad.losrios.edu>
2022-09-13 22:25:39 -0700

    first commit

--- code/Chaosgame.cpp
+++ code/Chaosgame.cpp
@@ -13,7 +13,38 @@ int main()
 
 	// Create and open a window for the game
 
-	RenderWindow window(vm, "Sierpinski Triangle!!", Style::Default);
+	RenderWindow window(vm, "Triangle", Style::Default);
+
+	bool paused = true;
+    // Draw some text
+    Text messageText;
+    // We need to choose a font
+    Font font;
+    font.loadFromFile("fonts/KOMIKAP_.ttf");
+    // Set the font to our message
+    messageText.setFont(font);
+    // Assign the actual message
+    messageText.setString("Click to start");
+    // Make it really big
+    messageText.setCharacterSize(75);
+
+    //Choose a color
+    messageText.setFillColor(Color::White);
+
+    // Position the text
+    FloatRect textRect = messageText.getLocalBounds();
+    messageText.setOrigin(textRect.left +
+        textRect.width / 2.0f,
+        textRect.top +
+        textRect.height / 2.0f);
+    messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+        
+    if (paused) {
+       // Draw our message
+       window.draw(messageText);
+    }
+    window.display();
+	
 	RectangleShape rect(Vector2f{20,10});
 
 	vector<Vector2f> vertices;   ///push_back stuff into us!
@@ -61,7 +92,7 @@ int main()
 		****************************************
 		*/
 		rect.setPosition(clicked.x, clicked.y);
-		rect.setFillColor(Color::Blue);
+		rect.setFillColor(Color::White);
 		/*
 		****************************************
 		Draw the scene
