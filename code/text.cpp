56fe9af16b638fb625d02a274535e018679a5091
admin <admin@BCS-Ubuntu-18.lrccd.ad.losrios.edu>
2022-09-13 21:52:32 -0700

    first commit

--- code/text.cpp
+++ code/text.cpp
@@ -8,20 +8,34 @@ using namespace std;
 
 int main()
 {
-// Draw some text
-   int score = 0;
-   Text messageText;
-   Text scoreText;
-   // We need to choose a font
-   Font font;
-   font.loadFromFile("fonts/KOMIKAP_.ttf");
-   // Set the font to our message
-   messageText.setFont(font);
-   scoreText.setFont(font);
-   // Assign the actual message
-   messageText.setString("Click anywhere to start");
-   scoreText.setString("Score = 0");
-   // Make it really big
-   messageText.setCharacterSize(75);
-   scoreText.setCharacterSize(100);
+    bool paused = true;
+    // Draw some text
+    Text messageText;
+    // We need to choose a font
+    Font font;
+    font.loadFromFile("fonts/KOMIKAP_.ttf");
+    // Set the font to our message
+    messageText.setFont(font);
+    // Assign the actual message
+    messageText.setString("Click to start");
+    // Make it really big
+    messageText.setCharacterSize(75);
+
+    //Choose a color
+    messageText.setFillColor(Color::White);
+
+    // Position the text
+    FloatRect textRect = messageText.getLocalBounds();
+    messageText.setOrigin(textRect.left +
+        textRect.width / 2.0f,
+        textRect.top +
+        textRect.height / 2.0f);
+    messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+        
+    if (paused) {
+       // Draw our message
+       window.draw(messageText);
+    }
+    window.display();
+    return 0;
 }
